a7f01b9e2cc4ff416e2f09b14efa25ce279ca412
julianandrescaracas0623 <andrescaracas0623@gmail.com>
2026-02-20 20:57:41 -0500

    fix: se modifico el codifo del servidor para que sea un servidor multicliente

--- servidor.py
+++ servidor.py
@@ -1,14 +1,32 @@
 import socket
+import threading
+
+
+def handle_client(conn, addr):
+    print('Conexion establecida desde {addr}')
+    
+    try:
+        student_name = conn.recv(1024).decode()
+        response  = f"Hola, {student_name} estas conectando un servidor concurrente!"
+        conn.sendall(response.encode())
+    except Exception as e:
+        print (f"Error con {addr}: {e}")
+    finally:
+        conn.close()
+        print(f"Conexion cerrada con {addr}")
+        
+# Configuracion del servidor socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 5000))
-server.listen(1)
+server.bind(("0.0.0.0", 5000))
+server.listen()
 
-print('Servidor esperando conexion tenga paciencia...')
+print("Servidor escuchando en el puerto...")
 
-conn, addr = server.accept()
-print(f'Conexion establecida desde {addr}')
-
-conn.sendall(b'Hola, Julian estas aprendiendo a programacion distribuida y has creado tu primer socket!')
-conn.close()
-
+while True:
+    conn, addr = server.accept()
+    
+# creamos un thread para manejar cada cliente de forma concurrente o hilos de ejecucion para manejar cada cliente de forma concurrente
+    client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+    client_thread.start()     
+     
